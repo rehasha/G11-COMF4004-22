555627b146db37256248d6d5c49d0f09e0eb1037
rasinduS17 <123339526+rasinduS17@users.noreply.github.com>
2023-01-23 22:08:50 +0530

    Add files via upload

---
+++ Jaal.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../../css/navfooter.css">
+    <link rel="stylesheet" href="../../css/salons.css">
+
+
+    <title>Document</title>
+</head>
+      <body>
+        <header>
+        <!-- header*************** -->
+        <div class="topArea">
+      <div class="header_Photo"> <img src="../../images/hair-salon-png-hd-hairdressing-u0026-styling-services-hairdressing-hd-png-1295.png" alt=""></div>
+        <div id="logo"> 
+          <img src="../../images/logo.jpg"> 
+        </div> 
+        
+         <!--NAV bar*************** -->
+          <nav class="navMenu">
+            <a href="../index.html">Home</a>
+            <a href="../find_a_salon.html">Salons</a>
+            <a href="../gallery.html">Gallery</a>
+            <a href="../2.html">Register</a>
+            <a href="../aboutUs.html">About</a>
+            
+            <form  class="sign" action="../signup.html">
+                <button class="but">Login</button>
+                
+                <button  class="but">SignUp</button>
+              </form>
+            
+            <div class="searchbar">
+            <form class="search" action="">
+                <input type="search" placeholder="Search here..." required>
+                <button type="submit">Search</button>
+              </form>
+            </div>
+          </nav>
+
+          <!--end of nav bar*************** -->
+      
+    
+    </header>
+ <!-- about us -->
+
+ <section class="sec1">
+    <div class="book-appointment">
+      <div class="sec1-image">
+            <img src="../../images/JS1.jpg" alt="" >
+        </div>
+
+        <div class="sec1-header">
+          <h2>Jaal Salon</h2>
+        </div>
+        
+        <div class="sec1-text">
+
+            Jaal Spa and Salon was founded by eminent personality Jalani Premadasa 
+            over a decade ago and offers an escape into a calming restorative oasis.Leading 
+            beauty store in Sri Lanka is Jaal Salons. Jaal is an outstanding spa that specializes
+            in offering genuinely opulent, one-of-a-kind experiences with its incredible variety of
+            different treatments and services.
+
+        </div>
+
+        <div>
+          <a href="../appointment.html">
+            <button  id="book_appoinment" > Book an appointment</button>
+          </a>
+        </div>
+        
+  
+    </div>
+ </section>
+
+ <section class="sec2">
+  
+    <div class="sec2-container">
+    <div class="sec2-image">
+      <img src="../../images/Js2.jpg" alt="" >
+    </div>
+    
+    
+    <div class="sec2-text">
+      <h1 class="sec2-header">ABOUT US</h1> <br><br>
+        <h2>
+            Our staff of highly trained beauticians at Jaal Spa and Salon will
+            meet all of your needs and give you a relaxing, life-changing experience. 
+            The peaceful refuge of Jaal, which is situated in Maitland Crescent Colombo 07 &
+            Shangri-La Hotel, provides a hasty getaway for urban dwellers seeking happiness.
+        </h2>
+    </div>
+
+   
+    
+  </div>
+
+ </section>
+
+ <section class="sec-3">
+  <div class="price_list-header"> 
+  <h1>PRICING <br> LIST</h1>
+ </div>
+
+ <div class="price_list-text">
+  <h3>Menicure & Pedicure..........................RS.1500</h3><br>
+  <h3>Facial.......................................RS.2000</h3><br>
+  <h3>Full body Waxing.............................RS.5000</h3><br>
+  <h3>Threading....................................RS.200</h3><br>
+  <h3>Haircut......................................RS.1000</h3><br>
+  <h3>Hair Styling(According to hair length).......RS.5000</h3><br>
+  <h3>Bridal Services Packages.....................RS.50,000</h3><br>
+  <h3>Party Makeup.................................RS.10,000</h3><br>
+  </div>
+ </section>
+
+
+      </body>
+      <!-- ***********FOOTER*************** -->
+      <footer class="footer">
+        <div class="footer__addr">
+          <h1 class="footer__logo">DIVINE</h1>
+
+          <address>
+            553 Colombo, Arthur st <br>
+                
+            <a class="footer__btn" href="mailto:example@gmail.com">Email Us</a>
+          </address>
+        </div>
+        
+        <ul class="footer__nav">
+          <li class="nav__item">
+            <h2 class="nav__title">ABOUT</h2>
+      
+            <ul class="nav__ul">
+              <li>
+                <a href="../aboutUs.html">About us</a>
+              </li>
+      
+              <li>
+                <a href="../career.html">Career</a>
+              </li>
+                  
+              <li>
+                <a href="../find_a_salon.html">Find a Salon</a>
+              </li>
+              <li>
+                <a href="../gallery.html">Gallery</a>
+              </li>
+             
+            </ul>
+          </li>
+          
+        
+       
+        </ul>
+        
+        
+          <p class="black">Stay informed about our new registered salons</p>
+        
+          <div class="legal">
+            <p>&copy; Group 11. All rights reserved.</p>
+          </div>
+        </footer>
+       
+     
+        
+
+</html>
+    
---
+++ gallery.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../css/navfooter.css">
+    <link rel="stylesheet" href="../gallery.css">
+
+
+    <title>Document</title>
+</head>
+      <body>
+        <header>
+        <!-- header*************** -->
+        <div class="topArea">
+      <div class="header_Photo"> <img src="../images/hair-salon-png-hd-hairdressing-u0026-styling-services-hairdressing-hd-png-1295.png" alt=""></div>
+        <div id="logo"> 
+          <img src="../images/logo.jpg"> 
+        </div> 
+        
+         <!--NAV bar*************** -->
+          <nav class="navMenu">
+            <a href="index.php">Home</a>
+            <a href="find_a_salon.php">Salons</a>
+            <a href="gallery.php">Gallery</a>
+            <a href="2.php">Register</a>
+            <a href="aboutUs.php">About</a>
+            
+            <form  class="sign" action="../html/signup.php">
+                <button class="but">Login</button>
+                
+                <button  class="but">SignUp</button>
+              </form>
+            
+            <div class="searchbar">
+            <form class="search" action="">
+                <input type="search" placeholder="Search here..." required>
+                <button type="submit">Search</button>
+              </form>
+            </div>
+          </nav>
+
+          <!--end of nav bar*************** -->
+          
+    
+    </header>
+ <!-- gallery -->
+ <div class="container">
+  <div class="heading">
+    <h3>Photo <span>Gallery</span></h3>
+  </div>
+  <div class="box">
+    
+    <div class="dream">
+      <img src="../images/5.jpg">
+       <img src="../images/G2.jpg">
+        <img src="../images/G3.jpg">
+         <img src="../images/G4.jpg">
+          <img src="../images/G5.jpg">
+          
+    </div>
+
+      <div class="dream">
+      <img src="../images/G6.jpg">
+       <img src="../images/G7.webp">
+        <img src="../images/G8.jpg">
+         <img src="../images/G9.jpg">
+          <img src="../images/G10.jpg">
+          
+    </div>
+
+      <div class="dream">
+      <img src="../images/2.jpg">
+       <img src="../images/black_girl.webp">
+        <img src="../images/Hair-Style-Mission-texas.webp">
+         <img src="../images/MS2.jpg">
+          <img src="../images/6.jpg">
+          
+    </div>
+
+
+
+
+  </div>
+
+</div>
+
+      </body>
+      <!-- ***********FOOTER*************** -->
+      <footer class="footer">
+        <div class="footer__addr">
+          <h1 class="footer__logo">DIVINE</h1>
+              
+          
+          <address>
+            553 Colombo, Arhtur st<br>
+                
+            <a class="footer__btn" href="mailto:example@gmail.com">Email Us</a>
+          </address>
+        </div>
+        
+        <ul class="footer__nav">
+          <li class="nav__item">
+            <h2 class="nav__title">ABOUT</h2>
+      
+            <ul class="nav__ul">
+              <li>
+                <a href="../html/aboutUs.php">About us</a>
+              </li>
+      
+              <li>
+                <a href="../html/career.php">Career</a>
+              </li>
+                  
+              <li>
+                <a href="../html/find_a_salon.php">Find a Salon</a>
+              </li>
+              <li>
+                <a href="../html/gallery.php">Gallery</a>
+              </li>
+             
+            </ul>
+          </li>
+          
+        
+       
+        </ul>
+        
+        <p class="black">Stay informed about our new registered salons</p>
+        
+        <div class="legal">
+          <p>&copy; Group 11. All rights reserved.</p>
+        </div>
+      </footer>
+     >
+     
+        
+
+</html>
+    
